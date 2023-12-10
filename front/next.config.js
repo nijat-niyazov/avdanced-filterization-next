fdd9b82b0d4360119e6052f46f95529526af9803
nidzhat-2000 <nidzhat.niyazov@gmail.com>
2023-12-10 11:59:32 +0400

    translates comes from backend

--- front/next.config.js
+++ front/next.config.js
@@ -11,4 +11,6 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+const withNextIntl = require("next-intl/plugin")("./src/i18n/i18n.ts");
+
+module.exports = withNextIntl(nextConfig);
